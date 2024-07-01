137b134a5a96ec83cbbadc0ff4cf704d53f93d31
BitContainer <BitContainer@gmail.com>
2024-07-01 21:21:02 +0000

    altering 100-puppet -> /etc/ssh/ssh_config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,7 @@
 # using puppet as a configuration manager (CM) to config our ssh user connection
 #
 # Create a file resource for the SSH client configuration file
-file { '~/.ssh/config':
+file { '/etc/ssh/ssh_config':
   # Ensure that the file is present and its type is 'file'
   ensure  => file,
   # Set the content of the file to the desired SSH client configuration
@@ -10,13 +10,7 @@ file { '~/.ssh/config':
 Host *
     IdentityFile ~/.ssh/school
     PasswordAuthentication no
-  ",
-  # Set the owner of the file to the specified username
-  owner   => 'ubuntu',
-  # Set the group of the file to the specified username
-  group   => 'ubuntu',
-  # Set the permissions of the file to read-only for the owner (0600)
-  mode    => '0600',
-}
+  "
+ }
 
 
