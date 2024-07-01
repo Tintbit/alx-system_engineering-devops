38f3e00e9265c6ecd03bd669b593816e82fb6dc0
BitContainer <BitContainer@gmail.com>
2024-07-01 21:12:40 +0000

    altering 100-puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,22 @@
 # File: 100-puppet_ssh_config.pp
-
-file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/ssh_config',
-  line  => '    PasswordAuthentication no',
-  match => '^#?[\s]*PasswordAuthentication',
+# using puppet as a configuration manager (CM) to config our ssh user connection
+#
+# Create a file resource for the SSH client configuration file
+file { '~/.ssh/config':
+  # Ensure that the file is present and its type is 'file'
+  ensure  => file,
+  # Set the content of the file to the desired SSH client configuration
+  content => "
+Host *
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+  ",
+  # Set the owner of the file to the specified username
+  owner   => 'ubuntu',
+  # Set the group of the file to the specified username
+  group   => 'ubuntu',
+  # Set the permissions of the file to read-only for the owner (0600)
+  mode    => '0600',
 }
 
-file_line { 'Declare identity file':
-  path  => '/etc/ssh/ssh_config',
-  line  => '    IdentityFile ~/.ssh/school',
-  match => '^#?[\s]*IdentityFile',
-}
+
---
+++ 0x0B-ssh/100-puppet_ssh_config.pp.alternativeSolution
@@ -0,0 +1,13 @@
+# File: 100-puppet_ssh_config.pp
+
+file_line { 'Turn off passwd auth':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    PasswordAuthentication no',
+  match => '^#?[\s]*PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path  => '/etc/ssh/ssh_config',
+  line  => '    IdentityFile ~/.ssh/school',
+  match => '^#?[\s]*IdentityFile',
+}
