538f72e90256fc75014968c3ef451f3f86cecd36
BitContainer <BitContainer@gmail.com>
2024-07-02 17:09:33 +0000

    commenting puppet manifest files

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,5 @@
+# this is a puppet manifset file that used to create and ensure the existance of file /tmp at path
+# puppet is and config manager (CM) tool. puppet uses `declarative` language to make an abstracted and `portable`  config files that can work/ used for different platforms.
 file { '/tmp':
   enusre     => file,
   path       => '/tmp/school',
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,3 +1,4 @@
+# this manifest file used to get flask via pip3
 package { 'flask':
   enusre   => present,
   provider => pip3,
