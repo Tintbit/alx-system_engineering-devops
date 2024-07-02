4222da7bfb15134aba8708828db001a7cc8815f9
BitContainer <BitContainer@gmail.com>
2024-07-02 16:55:16 +0000

    adding 0-create_afile.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+file { '/tmp':
+  enusre     => file,
+  path       => '/tmp/school',
+  permission => '0744',
+  owner      => 'www-data',
+  group      => 'www-data',
+  content    => 'I love Puppet',
+  }
