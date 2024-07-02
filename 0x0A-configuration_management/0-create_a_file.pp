7ce20072e7c45cdf5b230c15a8015f3a346aa8ae
BitContainer <BitContainer@gmail.com>
2024-07-02 17:51:16 +0000

    altering 0, adding killmenow file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # this is a puppet manifset file that used to create and ensure the existance of file /tmp at path
 # puppet is and config manager (CM) tool. puppet uses `declarative` language to make an abstracted and `portable`  config files that can work/ used for different platforms.
-file { '/tmp':
+file { '/tmp/school':
   enusre     => file,
-  path       => '/tmp/school',
-  permission => '0744',
+  mode       => '0744',
   owner      => 'www-data',
   group      => 'www-data',
   content    => 'I love Puppet',
