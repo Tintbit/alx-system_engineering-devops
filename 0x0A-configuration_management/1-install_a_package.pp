a9c3b439b24000fbc1d792dff0e5800bff052572
BitContainer <BitContainer@gmail.com>
2024-07-03 22:20:00 +0000

    altering 1-install_a....

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,23 +4,28 @@
 ##  provider => 'pip3',
 ##  }
 
-class { 'python':
-  ensure => present,
+#class { 'python':
+#  ensure => present,
+#  }
+#
+#package { 'python3-pip':
+#  ensure  => present,
+#  require => Class['python'],
+#  }
+#
+## Ensure pip3 is available
+#exec { 'Ensure pip3 is available':
+#  command => '/usr/bin/python3 -m ensurepip',
+#  unless  => '/usr/local/bin/pip3 --version',
+#  }
+#
+## Install Flask
+#package { 'flask==2.1.0':
+#  ensure   => present,
+#  provider => pip,
+#  }
+exec { 'Install Flask 2.1.0':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  onlyif  => '/usr/bin/pip3 show Flask >/dev/null || true',
   }
 
-package { 'python3-pip':
-  ensure  => present,
-  require => Class['python'],
-  }
-
-# Ensure pip3 is available
-exec { 'Ensure pip3 is available':
-  command => '/usr/bin/python3 -m ensurepip',
-  unless  => '/usr/local/bin/pip3 --version',
-  }
-
-# Install Flask
-package { 'flask==2.1.0':
-  ensure   => present,
-  provider => pip,
-  }
