089d03fd58172bbfa3201989ea451ca8dc0cc169
BitContainer <BitContainer@gmail.com>
2024-07-03 21:22:06 +0000

    altering 0,2 create afile and execute command

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # this is a puppet manifset file that used to create and ensure the existance of file /tmp at path
 # puppet is and config manager (CM) tool. puppet uses `declarative` language to make an abstracted and `portable`  config files that can work/ used for different platforms.
 file { '/tmp/school':
-  enusre     => file,
+  ensure     => file,
   mode       => '0744',
   owner      => 'www-data',
   group      => 'www-data',
