8a6d759df5538c852f544e05593e425bb3c23541
BitContainer <BitContainer@gmail.com>
2024-07-03 22:57:21 +0000

    ... install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,11 +3,11 @@
 package { 'python3.8':
   ensure   => 'python3.8.10',
   provider => 'pip3',
-  before   => Package['flask']
   }
 package { 'flask':
   enusre   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3.8']
   }
 
 
