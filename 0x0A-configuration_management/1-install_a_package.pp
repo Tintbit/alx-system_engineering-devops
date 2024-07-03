9a62f1497eb9233585183cc47b7c6e2261cc4a15
BitContainer <BitContainer@gmail.com>
2024-07-03 23:09:49 +0000

    alterig 1-..

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,21 @@
 # this manifest file used to get flask via pip3
 #
-package { 'python':
-  ensure   => '3.8.10',
-  provider => 'pip3',
-  }
-
+#package { 'python':
+#  ensure   => '3.8.10',
+#  provider => 'pip3',
+#  }
+#
+#package { 'flask':
+#  enusre   => '2.1.0',
+#  provider => 'pip3',
+#  }
+#
+#package { 'Werkzeug':
+#  ensure   => '2.1.1',
+#  provider => 'pip3',
+#  require  => Package['flask']
+#  }
 package { 'flask':
   enusre   => '2.1.0',
-  provider => 'pip3',
-  }
-
-package { 'Werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3',
-  require  => Package['flask']
+  provider => 'pip',
   }
