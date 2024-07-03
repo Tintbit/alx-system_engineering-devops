8efe272cebc4620695d508a52cba46c3eb039ca3
BitContainer <BitContainer@gmail.com>
2024-07-03 22:06:52 +0000

    altering 2-execute_a_command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -5,18 +5,18 @@
 ## then /usr/bin/pkill killmenow; fi: Executes /usr/bin/pkill killmenow to kill the process if it exists. 
 #The fi ends the conditional statement.
 
-#exec { 'check_and_kill_killmenow':
-#  command     => 'if ps aux | grep -q killmenow; then /usr/bin/pkill killmenow; fi',
-#  refreshonly => true,
-#}
-#
+exec { 'check_and_kill_killmenow':
+  command     => 'if ps aux | grep -q killmenow; then /usr/bin/pkill killmenow; fi',
+  refreshonly => true,
+}
+
 
 # uses the exec resource to run the pkill command, targeting processes named killmenow. 
 # The refreshonly => true parameter ensures that the command is idempotent, # meaning it won't cause issues if applied multiple times.
 # not idempotent (meaning it can be run multiple times without changing the outcome beyond the initial application).
 # 
-exec { 'killmenow':
-  command     => '/usr/bin/pkill killmenow',
-  refreshonly => true,
-}
+#exec { 'killmenow':
+#  command     => '/usr/bin/pkill killmenow',
+#  refreshonly => true,
+#}
 
