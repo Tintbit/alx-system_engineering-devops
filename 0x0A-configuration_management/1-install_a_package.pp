590ae0553ae49b92415ea1ed10800048b9e5941b
BitContainer <BitContainer@gmail.com>
2024-07-03 22:54:24 +0000

    install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,9 @@
 # this manifest file used to get flask via pip3
 #
 package { 'python3.8':
-  ensure => 'python3.8.10',
-  before => Package['flask']
+  ensure   => 'python3.8.10',
+  provider => 'pip3',
+  before   => Package['flask']
   }
 package { 'flask':
   enusre   => '2.1.0',
